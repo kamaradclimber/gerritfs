24b3e16e5721c46b6364186cb3fd0b6c345eb21f
Grégoire Seux <kamaradclimber@gmail.com>
2016-01-20 20:54:35 +0100

    Rename changes_fs into my_fs

---
+++ lib/gerritfs/fs/my_fs.rb
@@ -0,0 +1,27 @@
+module GerritFS
+  class MyFS
+    def initialize(gerrit, query)
+      @gerrit = gerrit
+      @query  = query
+      @dashboard = DashboardFS.new(gerrit, query)
+    end
+
+    def projects
+      @projects = @gerrit.projects
+      @projects # todo add clever caching
+    end
+
+    def elements
+      @elements ||= projects.each_with_object({}) do |pair, mem|
+        name, project = pair
+        url = @gerrit.clone_url_for(name)
+        mem[name.gsub('/', '_')] = ChangesFS.new(@gerrit, url)
+      end
+      @elements.merge({
+        dashboard: @dashboard
+      })
+    end
+
+    include CompositionFS
+  end
+end
